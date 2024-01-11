6b2abac486b754fc1885219312edb6a6dbc446b6
Alexandr <sa20sha05@gmail.com>
2024-01-12 01:15:06 +0300

    readd chine pc version

--- project_chine/dist/js/script.js
+++ project_chine/dist/js/script.js
@@ -7,8 +7,8 @@ $('.single-item').slick({
   slidesToShow: 1,
   infinite: true,
   adaptiveHeight: true,
-  prevArrow: '<button type="button" class="slick-prev"><img src="../img/reviews/Previous.png" class="carousel__prev" alt="prev"></button>',
-  nextArrow: '<button type="button" class="slick-next"><img src="../img/reviews/Next.png" class="carousel__next" alt="next"></button>',
+  prevArrow: '<img src="img/reviews/Previous.png" class="carousel__prev" alt="prev">',
+  nextArrow: '<img src="img/reviews/Next.png" class="carousel__next" alt="next">',
   responsive: [
     {
       breakpoint: 768,
