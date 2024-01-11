29575d7843a0cdf6c6152282d4a034b638d0b684
Alexandr <sa20sha05@gmail.com>
2024-01-12 01:49:40 +0300

    add end version pc chine

--- project_chine/src/js/script.js
+++ project_chine/src/js/script.js
@@ -32,4 +32,23 @@ $('.single-item').slick({
 });
 
 
+//плавный скролл
+
+
+document.querySelectorAll('.smooth-scroll').forEach(link => {
+  link.addEventListener('click', function(event) {
+    event.preventDefault();
+    const targetId = this.getAttribute('href');
+    const targetPosition = document.querySelector(targetId).offsetTop;
+
+    window.scroll({
+      top: targetPosition,
+      left: 0,
+      behavior: 'smooth'
+    });
+  });
+});
+
+
+
 
